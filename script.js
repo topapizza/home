d0ac387124dff91a6d07dc2c73edf81638ebdf92
topapizza <toivopek@gmail.com>
2024-10-25 12:48:01 +0300

    added a stopwatch for some reason

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
 const button = document.getElementById('cpsbutton');
 const h1 = document.getElementById('bestCPS');
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false; 
 
 cpsCounter = 0;
 bestCps = 0;
+
+/*---------Timer logic----------*/
+
+function start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(TimerUpdate, 10);
+        isRunning = true;
+    }
+    else{
+        stop();
+    }
+
+}
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now - startTime;
+        isRunning = false;
+    }
+}
+function reset(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false; 
+    display.textContent = "00:00:00";
+}
+function TimerUpdate(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${minutes}:${seconds}:${milliseconds}`;
+}
+
+
+
+/*---------CPS logic----------*/
 
 button.addEventListener('click', function() {
     cpsCounter ++;
@@ -30,6 +83,8 @@ requestAnimationFrame(update);
 
 
 
+/*---------Button logic----------*/
+
 function goToYT(){
     window.location.href = "https://www.youtube.com";
 }
@@ -53,3 +108,6 @@ function goToCopilot(){
 function goToItchIO(){
     window.location.href = "https://itch.io";
 }
+
+
+
